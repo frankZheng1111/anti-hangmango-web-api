6b55f6ebfa9128ec0bb7990b23d9be562b6b4d6c
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-27 15:42:46 +0800

    Add Hangman struct

--- model/hangman.go
+++ model/hangman.go
@@ -5,9 +5,24 @@ import (
 	"log"
 )
 
-func UserNewHangman(user *User) {
-	res, _ := api.NewHangman(user.AuthToken)
-	resBodyJSON, _ := res.ParseBodyToJSON()
-	log.Println("New Hangman success: ", string(resBodyJSON))
-	return
+type Hangman struct {
+	Id            int64
+	Hp            int8
+	Word          string
+	StaticLetters []map[rune]int
+}
+
+func UserNewHangman(user *User) (*Hangman, error) {
+	res, err := api.NewHangman(user.AuthToken)
+	if err != nil {
+		return nil, api.BaseAPIRespErrorHandle(res, err)
+	}
+	defer res.Body.Close()
+	resBodyMap, _ := res.ParseBodyToMap()
+	hangman := new(Hangman)
+	hangman.Id = int64(resBodyMap["id"].(float64))
+	hangman.Hp = int8(resBodyMap["hp"].(float64))
+	hangman.Word = resBodyMap["word"].(string)
+	log.Println("New Hangman success: ", hangman)
+	return hangman, nil
 }
