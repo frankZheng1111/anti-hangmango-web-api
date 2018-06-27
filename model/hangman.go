453ac0eb9f8e2cd6da3caf065d7307afa940be19
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-27 17:23:48 +0800

    Add Hangman#InitDictionary

--- model/hangman.go
+++ model/hangman.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"anti-hangmango-web-api/api"
+	"anti-hangmango-web-api/config"
 	"log"
 )
 
@@ -23,6 +24,24 @@ func UserNewHangman(user *User) (*Hangman, error) {
 	hangman.Id = int64(resBodyMap["id"].(float64))
 	hangman.Hp = int8(resBodyMap["hp"].(float64))
 	hangman.Word = resBodyMap["word"].(string)
+	hangman.InitDictionary()
 	log.Println("New Hangman success: ", hangman)
 	return hangman, nil
+}
+
+func (hangman *Hangman) InitDictionary() {
+	wordLen := len([]rune(hangman.Word))
+	hangman.StaticLetters = make([]map[rune]int, wordLen)
+	for i := 0; i < wordLen; i++ {
+		hangman.StaticLetters[i] = make(map[rune]int, 26)
+	}
+	for _, word := range config.Config.Hangman.Dictionary {
+		letterRunes := []rune(word)
+		if wordLen != len(letterRunes) {
+			continue
+		}
+		for index, letterRune := range letterRunes {
+			hangman.StaticLetters[index][letterRune]++
+		}
+	}
 }
