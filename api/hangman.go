6288541a2bc1d955e44a7c11385fa7f5077bca5d
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-27 17:30:38 +0800

    Add api.HangmanGuessALetter

--- api/hangman.go
+++ api/hangman.go
@@ -2,9 +2,18 @@ package api
 
 import (
 	"anti-hangmango-web-api/config"
+	"fmt"
 )
 
 func NewHangman(token string) (*Response, error) {
 	res, err := Post(config.Config.APIUrl+"/v1/hangmen", map[string]string{"hangmango-auth-token": token}, nil)
+	return res, err
+}
+
+func HangmanGuessALetter(token string, hangmanId int64, letter string) (*Response, error) {
+	url := fmt.Sprintf("%s/v1/hangmen/%d/guess", config.Config.APIUrl, hangmanId)
+	header := map[string]string{"hangmango-auth-token": token}
+	body := map[string]interface{}{"letter": letter}
+	res, err := Post(url, header, body)
 	return res, err
 }
