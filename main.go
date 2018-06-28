503a99d6ae26552a6156e3a3722eb278671e45b7
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-28 14:36:05 +0800

    minor change

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"anti-hangmango-web-api/game"
-	"anti-hangmango-web-api/model"
+	// "anti-hangmango-web-api/model"
 )
 
 func main() {
-	model.BestUsers(1, 30)
+	// model.BestUsers(1, 30)
 	game.Start()
 }
